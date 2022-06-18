2491b018eeb271609d302d2d9fcd03cd41d8db46
Omotayo Ganiyu Seedik <60110209+Tdaycode@users.noreply.github.com>
2022-06-18 22:58:15 +0100

    adding migrations

--- I4G032786SKI/blog/models.py
+++ I4G032786SKI/blog/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth import get_user_model
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=200, string=True)
+    title = models.CharField(max_length=200)
     text = models.TextField()
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now_add=True)
