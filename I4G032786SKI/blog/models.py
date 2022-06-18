827710913ce4057301fb8493ce81111ad6f3b5ab
Omotayo Ganiyu Seedik <60110209+Tdaycode@users.noreply.github.com>
2022-06-18 16:32:58 +0100

    finsishing

--- I4G032786SKI/blog/models.py
+++ I4G032786SKI/blog/models.py
@@ -4,4 +4,8 @@ from django.db import models
 
 
 class Post:
-    pass
+    title = models.CharField(max_length=200, string=True)
+    text = models.TextField()
+    author = models.get_user_model()
+    created_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(blank=True, null=True)
