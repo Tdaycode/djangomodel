919cec1993f574b41a1ded32643e9cc5d7fb1d0b
Omotayo Ganiyu Seedik <60110209+Tdaycode@users.noreply.github.com>
2022-06-18 21:24:48 +0100

    adding

--- I4G032786SKI/blog/models.py
+++ I4G032786SKI/blog/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 
 
-class Post:
+class Post(models.Model):
     title = models.CharField(max_length=200, string=True)
     text = models.TextField()
-    author = models.get_user_model()
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now_add=True)
     published_date = models.DateTimeField(blank=True, null=True)
